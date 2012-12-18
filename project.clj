2e7a26d4ff1c2588c0ecf7cf7f47ac581a756e32
Matthew Smith <m0smith@yahoo.com>
2012-12-18 14:18:45 -0700

    updated project info

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [stencil "0.3.1"]]
   :plugins [[lein-pprint "1.1.1"]]
-  :eval-in-leiningen true)
+  :scm {:url "git@github.com:m0smith/lein-stencil.git"}
+  :url "https://github.com/m0smith/lein-stencil"
+  :eval-in-leiningen true
+  :pom-addition [:developers [:developer
+                              [:name "Matthew O. Smith"]
+                              [:url "http://m0smith.com"]
+                              [:email "matt@m0smith.com"]
+                              [:timezone "-7"]]]))
