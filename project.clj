0a34e1a5aaa556ca923e023fada0ed83734435a2
Matthew O. Smith <matt@m0smith.com>
2014-07-16 16:58:08 -0600

    gh-11 stencil is included in lein so I followed their example and excluded core.cache

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
-(defproject lein-resource "0.3.6" 
+
+(defproject lein-resource "0.3.7" 
   :description "lein-resource: a lein plugin to copy resources and apply a stencil transformation on them"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [bultitude "0.2.4"]  ;; namespace finder
-                 [org.clojure/test.check "0.5.7"] ;; property testing
-                 [stencil "0.3.3"]]
+                 [org.clojure/data.priority-map "0.0.5"]
+                 [org.clojure/test.check "0.5.8"] ;; property testing
+                 [stencil "0.3.3" :exclusions [org.clojure/core.cache]]
+                 ]
   :plugins [[lein-pprint "1.1.1"]]
   :scm {:url "git@github.com:m0smith/lein-resource.git"}
   :url "https://github.com/m0smith/lein-resource"
